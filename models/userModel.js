26fbec9a3d4f5fba0164203144d44d7e84c0fea3
nikhil singh nikhil singh <nikhilbhu32@gmail.com>
2024-01-10 07:52:13 -0800

    create mvc pattern

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+    },
+    role: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    question: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+export default mongoose.model('users', userSchema);
