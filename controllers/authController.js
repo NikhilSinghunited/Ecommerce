4e7ddb868d9366e1b743c3ce40356884bf91a8c2
nikhil singh nikhil singh <nikhilbhu32@gmail.com>
2024-01-12 06:11:16 -0800

    login api added

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,3 +43,54 @@ export const registerController = asyncHandler(async (req, res) => {
     });
   }
 });
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(req.body);
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        mssage: 'Invalid email or password',
+      });
+      console.log(res);
+    }
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(400).send({
+        sucess: false,
+        message: 'Email is not registered',
+      });
+    }
+    const match = await comparePassword(password, user.password);
+    console.log(match);
+    if (!match) {
+      return res.status(200).send({
+        sucess: false,
+        message: 'Invalid Passord',
+      });
+    }
+    //token generate
+    const token = await JWT.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '365d',
+    });
+    res.status(200).send({
+      sucess: true,
+      message: 'login sucessfully',
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+        role: user.role,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      sucess: False,
+      message: 'Erro in Login',
+      error,
+    });
+  }
+};
